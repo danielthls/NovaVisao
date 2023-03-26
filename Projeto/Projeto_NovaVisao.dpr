0f03c4d9a034255f90218d534f096e3cba5dd3a7
DESKTOP-S4BD7LK\Ultrabook Lenovo <daniel_thls@yahoo.com.br>
2023-03-26 10:41:57 -0300

    torna botão de pre-visualizar imagem disponível

--- Projeto/Projeto_NovaVisao.dpr
+++ Projeto/Projeto_NovaVisao.dpr
@@ -3,29 +3,29 @@ program Projeto_NovaVisao;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  UfrmHome in 'views\UfrmHome.pas' {frmHome},
-  UUtils.Enum in 'Utils\UUtils.Enum.pas',
-  UfrmEnviar in 'views\UfrmEnviar.pas' {Form1},
-  UService.Imagem in 'service\UService.Imagem.pas',
-  UfrmCadastrar in 'views\UfrmCadastrar.pas' {frmCadastro},
-  UfrmSistema in 'views\UfrmSistema.pas' {frmSistema},
-  UfrmImagens in 'views\UfrmImagens.pas' {frmImagens},
-  UService.ChatGPT in 'service\UService.ChatGPT.pas',
-  UDao.Cliente in 'Dao\UDao.Cliente.pas',
-  uEntity.Cliente in 'Entities\uEntity.Cliente.pas',
-  UUtils.Banco in 'Utils\UUtils.Banco.pas',
-  UUtils.Consts in 'Utils\UUtils.Consts.pas',
-  UDM in 'service\UDM.pas' {DM: TDataModule},
-  UService.NotificacaoApp in 'service\UService.NotificacaoApp.pas',
-  UService.NotificacaoEmail in 'service\UService.NotificacaoEmail.pas',
-  UUtils.Notificacao in 'Utils\UUtils.Notificacao.pas';
+  UDao.Cliente in 'Model\Dao\UDao.Cliente.pas',
+  uEntity.Cliente in 'Model\Entities\uEntity.Cliente.pas',
+  UDM in 'Model\service\UDM.pas' {DM: TDataModule},
+  UService.ChatGPT in 'Model\service\UService.ChatGPT.pas',
+  UService.Imagem in 'Model\service\UService.Imagem.pas',
+  UService.NotificacaoApp in 'Model\service\UService.NotificacaoApp.pas',
+  UService.NotificacaoEmail in 'Model\service\UService.NotificacaoEmail.pas',
+  UUtils.Banco in 'Model\Utils\UUtils.Banco.pas',
+  UUtils.Consts in 'Model\Utils\UUtils.Consts.pas',
+  UUtils.Enum in 'Model\Utils\UUtils.Enum.pas',
+  UUtils.Notificacao in 'Model\Utils\UUtils.Notificacao.pas',
+  UfrmCadastrar in 'Model\views\UfrmCadastrar.pas' {frmCadastrar},
+  UfrmEnviar in 'Model\views\UfrmEnviar.pas' {frmEnviar},
+  UfrmHome in 'Model\views\UfrmHome.pas' {frmHome},
+  UfrmImagens in 'Model\views\UfrmImagens.pas' {frmImagens},
+  UfrmSistema in 'Model\views\UfrmSistema.pas' {frmSistema};
 
 {$R *.res}
 
 begin
-  //ReportMemoryLeaksOnShutdown := True;
+  ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
-  Application.CreateForm(TfrmHome, frmHome);
   Application.CreateForm(TDM, DM);
+  Application.CreateForm(TfrmHome, frmHome);
   Application.Run;
 end.
